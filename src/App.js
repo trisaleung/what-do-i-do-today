86d39ac6d3cc1648596563dfb4ecd265079752e9
Trisa Leung <trisaleung1812@gmail.com>
2020-03-23 18:47:31 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="wholePage">
+
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
+    </style>
+
+      <h1 className="mainTitle">what do i do today?</h1>
+
+    </div>
+  );
+}
+
+export default App;
