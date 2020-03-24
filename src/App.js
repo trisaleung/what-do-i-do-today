4862fd0f226c50fbdb9c379e85b50de20e1b00a1
Trisa Leung <trisaleung1812@gmail.com>
2020-03-23 19:21:30 -0700

    made topbar sticky

--- src/App.js
+++ src/App.js
@@ -9,18 +9,20 @@ function App() {
         @import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
         @import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
     </style>
+    <div className="logo">
+      <div className="logoTitle">nudge</div>
+    </div>
 
       <h1 className="mainTitle">what do i do today?</h1>
 
       <p>button goes here to scroll down</p>
 
-      <p className="question">question1</p>
-      <p className="question">question2</p>
-      <p className="question">question3</p>
-      <p className="question">question4</p>
-      <p className="question">question5</p>
+      <p className="question">do you want to stay indoors or outdoors?</p>
+      <p className="question">how tired are you?</p>
+      <p className="question">what time is it?</p>
+      <p className="question">some hobbies are:</p>
 
-      <p className="result">result</p>
+      <p className="result">result!!</p>
     </div>
   );
 }
