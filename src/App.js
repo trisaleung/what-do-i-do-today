f7a225e0d29db68e21d2d4d348788a76f2b7ac09
Trisa Leung <trisaleung1812@gmail.com>
2020-03-23 18:58:07 -0700

    outline

--- src/App.js
+++ src/App.js
@@ -7,10 +7,20 @@ function App() {
 
     <style>
         @import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
+        @import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
     </style>
 
       <h1 className="mainTitle">what do i do today?</h1>
 
+      <p>button goes here to scroll down</p>
+
+      <p className="question">question1</p>
+      <p className="question">question2</p>
+      <p className="question">question3</p>
+      <p className="question">question4</p>
+      <p className="question">question5</p>
+
+      <p className="result">result</p>
     </div>
   );
 }
