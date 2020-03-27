cc2e21a8c3d5f8fbafa9eb4b9b242eecce2f1bd2
Trisa Leung <trisaleung1812@gmail.com>
2020-03-26 19:50:51 -0700

    removed slider api

--- src/App.js
+++ src/App.js
@@ -35,11 +35,19 @@ class App extends React.Component {
     this.refs.q4.scrollIntoView({behavior: "smooth"});
   };
 
-  handleOnChange = (value) => {
+  handleChangeStart = () => {
+    console.log('Change event started')
+  };
+
+  handleChange = value => {
     this.setState({
-      volume: value
+      value: value
     })
-  }
+  };
+
+  handleChangeComplete = () => {
+    console.log('Change event completed')
+  };
 
   handleSubmit = formSubmitEvent => {
     formSubmitEvent.preventDefault();
@@ -48,7 +56,7 @@ class App extends React.Component {
   };
 
   render() {
-    var { volume } = this.state
+    var { value } = this.state
 
     return (
       <div className="wholePage">
@@ -58,66 +66,117 @@ class App extends React.Component {
           @import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
       </style>
       <div className="logo">
-        <div className="logoTitle">nudge</div>
+        <div className="logoTitle">
+          nudge
+        </div>
       </div>
 
-        <h1 className="mainTitle"><ReactTypingEffect speed="100" text="what do i do today?" /></h1>
+        <h1 className="mainTitle">
+          <ReactTypingEffect 
+            speed="100" 
+            text="what do i do today?" />
+        </h1>
 
-        <button className="next-button" type="button" onClick={this.handleClickStart}>
+        <button 
+          className="next-button" 
+          type="button" 
+          onClick={this.handleClickStart}>
             start
         </button>
 
-        <div className="break" ref="q1"></div>
+        <div className="break" 
+          ref="q1" />
 
-        <form onSubmit={this.handleSubmit} className="form">
+        <form 
+          onSubmit={this.handleSubmit} 
+          className="form">
 
-          <p className="question">do you want to stay indoors or outdoors?</p>
+          <p className="question">
+            do you want to stay indoors or outdoors?
+          </p>
             <label className="q1Input">
-              <input type="radio" name="question-one-a" value="indoors" className="inputRadio"
-                 checked={this.state.selectedOption === "indoors"}
-                 onChange={this.handleOptionChange}
+              <input 
+                type="radio" 
+                name="question-one-a" 
+                value="indoors" 
+                className="inputRadio"
+                checked={this.state.selectedOption === "indoors"}
+                onChange={this.handleOptionChange}
               />
               indoors
             </label>
             <br /><br />
             <label className="q1Input">
-              <input type="radio" name="question-one-b" value="outdoors" className="inputRadio"
+              <input 
+                type="radio" 
+                name="question-one-b" 
+                value="outdoors" 
+                className="inputRadio"
                 checked={this.state.selectedOption === "outdoors"}
                 onChange={this.handleOptionChange}
               />
               outdoors
             </label>
             <br /><br />
-            <button className="next-button" type="button" onClick={this.handleClickQ1}>
+            <button 
+              className="next-button" 
+              type="button" 
+              onClick={this.handleClickQ1}>
                next
             </button>
 
-          <div className="break" ref="q2"></div>
+          <div className="break" 
+            ref="q2"/>
 
-          <p className="question">how tired are you?</p>
+          <p className="question">
+            how tired are you?
+          </p>
+          <div className="slider">
             <Slider
-              value={volume}
+              min={0}
+              max={10}
+              step={1}
+              value={value}
               orientation="vertical"
-              onChange={this.handleOnChange}
+              onChangeStart={this.handleChangeStart}
+              onChange={this.handleChange}
+              onChangeComplete={this.handleChangeComplete}
             />
+          </div>
 
-            <button className="next-button" type="button" onClick={this.handleClickQ2}>
+            <button
+              className="next-button" 
+              type="button" 
+              onClick={this.handleClickQ2}>
                next
             </button>
 
-          <div className="break" ref="q3"></div>
+          <div 
+            className="break" 
+            ref="q3" />
 
-          <p className="question">what time is it?</p>
-            <button className="next-button" type="button" onClick={this.handleClickQ3}>
+          <p className="question">
+            what time is it?
+          </p>
+            <button 
+              className="next-button" 
+              type="button" 
+              onClick={this.handleClickQ3}>
                next
             </button>
 
 
-          <div className="break" ref="q4"></div>
+          <div 
+            className="break" 
+            ref="q4" />
 
-          <p className="question">some hobbies are:</p>
+          <p className="question">
+            some hobbies are:
+          </p>
         </form>
-        <p className="result">result!!</p>
+        <p className="result">
+          result!!
+        </p>
       </div>
     );
   }
